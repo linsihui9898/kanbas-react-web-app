fc0a000047f32aa0769b7f77bd79a026c7599ce9
linsihui9898 <lin.sih@northeastern.edu>
2023-12-07 18:37:43 -0500

    a5

--- public/labs/a5/WorkingWithObjects.js
+++ public/labs/a5/WorkingWithObjects.js
@@ -8,6 +8,12 @@ function WorkingWithObjects() {
          className="btn btn-primary me-2">
         Get Assignment
       </a>
+      <h4>Retrieving Properties</h4>
+      <a
+        href="http://localhost:4000/a5/assignment/title"
+        className="btn btn-primary me-2">
+        Get Title
+      </a>
     </div>
   );
 }
